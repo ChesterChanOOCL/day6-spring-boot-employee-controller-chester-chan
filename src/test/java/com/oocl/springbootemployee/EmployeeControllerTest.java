9aad317cae4672f26ca9d45fb1ab62a00795619a
CHANCH10 <chester.chan@oocl.com>
2024-11-25 19:42:13 +0800

    Test : should return all employee when get all employee given exist

--- src/test/java/com/oocl/springbootemployee/EmployeeControllerTest.java
+++ src/test/java/com/oocl/springbootemployee/EmployeeControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static com.oocl.springbootemployee.commmon.Gender.Male;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -51,51 +52,4 @@ class EmployeeControllerTest {
         assertThat(result).usingRecursiveComparison().isEqualTo(employees);
     }
 
-    @Test
-    public void should_an_Employee_when_get_by_id_given_an_existing_id() throws Exception {
-        //Given
-        Employee expectEmployee = employeeRepository.getAll().get(0);
-        //When
-        String response = client.perform(MockMvcRequestBuilders.get("/employee/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn().getResponse().getContentAsString();
-        //Then
-        Employee result = jacksonTester.parseObject(response);
-        assertEquals(expectEmployee.getId(),result.getId());
-    }
-
-    @Test
-    public void should_return_employee_when_get_by_gender_given_gender() throws Exception {
-        //Given
-        List<Employee> employees = employeeRepository.getAll();
-        //When
-        String response = client.perform(MockMvcRequestBuilders.get("/employee").param("gender","Male"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(2)))
-                .andReturn().getResponse().getContentAsString();
-        //Then
-        List<Employee> resultList = jacksonList.parseObject(response);
-        assertEquals(employees.get(0).getId(),resultList.get(0).getId());
-        assertEquals(employees.get(2).getId(),resultList.get(1).getId());
-    }
-
-    @Test
-    public void should_return_employee_when_create_employee_given_employee() throws Exception {
-        //Given
-        String employee = "{\n" +
-                "    \"name\": \"test\",\n" +
-                "    \"age\": 28,\n" +
-                "    \"gender\": \"Male\",\n" +
-                "    \"salary\": 3000\n" +
-                "}";
-        //When
-        client.perform(MockMvcRequestBuilders.post("/employee").contentType(MediaType.APPLICATION_JSON).content(employee))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("test"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(28))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value("Male"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(new BigDecimal(3000)));
-        //Then
-
-    }
 }
